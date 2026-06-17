f26e581fc92cccf9e0843c854818fedfade0cde9
Ashutosh kumar Choudhary <ashutoshchoudhary0310@gmail.com>
2026-06-17 19:50:57 +0530

    Fixed Prompt_ui.py and working chatbot

--- Prompt_ui.py
+++ Prompt_ui.py
@@ -1,74 +1,3 @@
-# import streamlit as st
-# from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# st.set_page_config(
-#     page_title="AI Chatbot",
-#     page_icon="🤖",
-#     layout="centered"
-# )
-
-# st.title("🤖 AI Chatbot")
-
-# if st.sidebar.button("➕ New Chat"):
-#     st.session_state.messages = []
-#     st.rerun()
-
-# if "messages" not in st.session_state:
-#     st.session_state.messages = []
-
-# llm = HuggingFaceEndpoint(
-#     repo_id="Qwen/Qwen2.5-7B-Instruct",
-#     task="text-generation",
-#     max_new_tokens=512,
-#     temperature=0.7
-# )
-
-# model = ChatHuggingFace(llm=llm)
-
-# for message in st.session_state.messages:
-#     with st.chat_message(
-#         message["role"],
-#         avatar="😎" if message["role"] == "user" else "🤖"
-#     ):
-#         st.write(message["content"])
-
-# user_input = st.chat_input("Ask anything...")
-
-# if user_input:
-
-#     with st.chat_message("user", avatar="😎"):
-#         st.write(user_input)
-
-#     st.session_state.messages.append(
-#         {"role": "user", "content": user_input}
-#     )
-
-#     conversation = ""
-
-#     for msg in st.session_state.messages:
-#         role = msg["role"]
-#         content = msg["content"]
-
-#         if role == "user":
-#             conversation += f"User: {content}\n"
-#         else:
-#             conversation += f"Assistant: {content}\n"
-
-#     with st.spinner("Thinking..."):
-#         result = model.invoke(conversation)
-#         response = result.content
-
-#     with st.chat_message("assistant", avatar="🤖"):
-#         st.write(response)
-
-#     st.session_state.messages.append(
-#         {"role": "assistant", "content": response}
-#     )
-
-
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -78,20 +7,10 @@ from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 load_dotenv()
 
-<<<<<<< HEAD
-st.set_page_config(
-    page_title="AI Satbot",
-    page_icon="🤖",
-    layout="centered"
-)
-
-st.title("🤖 AI Satbot")
-=======
 tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
 st.set_page_config(page_title="SatBot", page_icon="🤖", layout="centered")
 st.title("🤖 SatBot")
->>>>>>> 94c8ae1 (Updated Prompt_ui.py)
 
 if st.sidebar.button("➕ New Chat"):
     st.session_state.messages = []
@@ -195,10 +114,4 @@ use it when answering.
     with st.chat_message("assistant", avatar="🤖"):
         st.write(response)
 
-<<<<<<< HEAD
-    st.session_state.messages.append(
-        {"role": "assistant", "content": response}
-    )
-=======
     st.session_state.messages.append({"role": "assistant", "content": response})
->>>>>>> 94c8ae1 (Updated Prompt_ui.py)
